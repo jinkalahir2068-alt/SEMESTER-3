739799a75a5de8f5d612686c756acd4d5792ad5c
Jinkal Makwana <jinkalahir2068@gmail.com>
2026-08-12 23:26:00 +0530

    Refactor stack implementation with improved structure

--- Data Structure/Practical-4/stack_implementation.c
+++ Data Structure/Practical-4/stack_implementation.c
@@ -1,69 +1,109 @@
-//stack implementation using array.
-#include<stdio.h>
-#define n 5
+//Stack implementation using Array
+#include <stdio.h>
+#define N 5
+int TOP = -1;
+int S[N];
 
-int TOP=-1;
-int S[n];
-
-void push(int x)
+void push (int x)
 {
-    if(TOP>=n-1){
-        printf("stack overflow");
-        return;
-    }
-    S[++TOP]=x;
-}
-
-int pop()
-{
-    if(TOP == -1){
-        printf("stack underflow");
-        return;
-    }
-    return S[TOP--];
-}
-
-int peek(){
-    if(TOP==-1){
-        printf("stack underflow");
-        return;
-    }
-    return S[TOP];
-}
-
-int peep(int i){
-    if((TOP-i+1) == -1){
-        printf("element not availiable");
-        return;
-    }
-    return S[(TOP-i+1)];
-}
-
-void change(int x,int i){
-    if((TOP-i+1)==-1){
-        printf("element not availiable");
-        return;
-    }
-    S[(TOP-i+1)]=x;
-}
-void display(){
-    if(TOP ==-1)
+    if(TOP>=N-1)
     {
-        printf("Stack Underflow.");
-        return;
+        printf("Stack Overflow\n");
     }
-        for(int i=TOP; i>=0; i--){
-        printf("%d\t", S[i]);
+    else
+    {
+        TOP++;
+        S[TOP] = x;
+        printf("%d pushed to stack\n", x);
     }
 }
-void main(){
-    push(30);
-    push(40);
-    push(50);
-    push(60);
-    push(70);
-    printf("Top element (peek): %d\n", peek()); 
-    printf("2nd element from top (peep): %d\n", peep(2)); 
-    change(100, 3);
-    display();
+void pop()
+{
+    if(TOP<=-1)
+    {
+        printf("Stack Underflow\n");
+    }
+    TOP--;
+    printf("%d popped from stack\n", S[TOP+1]);
+}
+int peep(int position)
+{
+    if(TOP-position+1<=-1)
+    {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    else
+    {
+       printf("Element at position %d is %d\n", position, S[TOP-position+1]);
+        return S[TOP-position+1];
+    }
+}
+void change(int position, int x)
+{
+    if(TOP-position+1<=-1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        S[TOP-position+1] = x;
+        printf("Element at position %d changed to %d\n", position, x);
+    }
+}
+void display()
+{
+    if(TOP>=0)
+    {
+        printf("Stack elements are:\n");
+        for(int i=TOP; i>=0; i--)
+        {
+            printf("%d\n", S[i]);
+        }
+    }
+    else
+    {
+        printf("Stack is empty\n");
+    }
+}
+void main()
+{
+    int choice, x;
+    while(1)
+    {
+        printf("1. Push\n2. Pop\n3. Display\n4. Peep\n5. Change\n6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Enter the element to push: ");
+                scanf("%d", &x);
+                push(x);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("Enter the position to peep: ");
+                scanf("%d", &x);
+                peep(x);
+                break;
+            case 5:
+                printf("Enter the position to change: ");
+                scanf("%d", &x);
+                printf("Enter the new value: ");
+                int newValue;
+                scanf("%d", &newValue);
+                change(x, newValue);
+                break;
+            case 6:
+                return;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 }
