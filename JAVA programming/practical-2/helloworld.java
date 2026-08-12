a5583a37d4bf12426fd85f1d11e7b8d8d8f89bb9
Jinkal Makwana <jinkalahir2068@gmail.com>
2026-08-12 23:16:05 +0530

    Add HelloWorld program to print 'Hello World'

---
+++ JAVA programming/practical-2/helloworld.java
@@ -0,0 +1,8 @@
+//Write a java program to print Hello World.[A]
+import java.util.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");
+    }
+}
