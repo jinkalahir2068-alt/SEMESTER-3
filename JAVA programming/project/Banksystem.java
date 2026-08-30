23ace6d1cae117d290e3d9cb5339cb4a21a24b1d
Jinkal Makwana <jinkalahir2068@gmail.com>
2026-08-30 11:19:50 +0530

    Refactor Banksystem class and methods
    Refactor Banksystem class for improved readability and functionality. Changed access modifiers, updated method signatures, and enhanced user prompts.

--- JAVA programming/project/Banksystem.java
+++ JAVA programming/project/Banksystem.java
@@ -1,93 +1,132 @@
 import java.util.Scanner;
 
-public class Banksystem{
+public class Banksystem
+{
     private String accno;
     private Double balance;
-    public String name;
+    String name;
+    static int count = 0;
 
-    public Banksystem(String accno, Double balance, String name){
-        this.accno=accno;
-        this.balance=balance;
-        this.name=name;
+    Banksystem(String b_accno, Double b_balance, String b_name)
+    {
+        this.accno = b_accno;
+        this.balance = b_balance;
+        this.name = b_name;
+        count++;
     }
 
-    public void checkBalance(){
-        System.out.println("your account details::");
-        System.out.println("Account number: "+ accno);
-        System.out.println("currentbalance: "+ balance);
-        System.out.println("Account holder name: "+ name);
+    void checkBalance()
+    {
+        System.out.println("Your account details:");
+        System.out.println("Account number: " + accno);
+        System.out.println("Current balance: " + balance);
+        System.out.println("Account holder name: " + name);
     }
 
-    public void depositMoney(double damount){
-        if(damount >0){
-            balance=balance+damount;
-            System.out.println("deposit sucessfull!! "+ damount);
-            System.out.println("your new balance: "+ balance);
+    void depositMoney(double damount)
+    {
+        balance += damount;
+        System.out.println("Deposit successful!! " + damount
+                + " Your new balance: " + balance);
+    }
+
+    public void withdrawMoney(double wamount)
+    {
+        if (wamount <= balance)
+        {
+            balance -= wamount;
+            System.out.println("Withdraw successful!! " + wamount
+                    + " Your remaining balance: " + balance);
         }
-        else{
-            System.out.println("amount must be greater then zero");
-        }
-    }
-
-    public void withdrawMoney(double wamount){
-        if(wamount > balance){
+        else
+        {
             System.out.println("Transaction Failed! Insufficient balance");
         }
-        else if(wamount <= 0){
-            System.out.println("amount must be greater then zero");
-        }
-        else{
-            balance =balance-wamount;
-            System.out.println("withdraw sucessfull!! "+ wamount);
-            System.out.println("your remaining balance: "+ balance);
-        }
     }
 
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        
-        Banksystem myaccount= new Banksystem( "123456789",10000.0, "jinkal makwana");
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
 
-        int choice=0;
+       
+        String username = "jinkal";
+        String password = "mybank20";
 
-        System.out.println("welcome to the banking system "+ myaccount.name);
+        System.out.print("Enter username: ");
+        String inputUsername = sc.nextLine();
 
-        while(choice != 4){
+        System.out.print("Enter password: ");
+        String inputPassword = sc.nextLine();
 
-            System.out.println("1. Check Balance");
+        if (!inputUsername.equals(username) && !inputPassword.equals(password))
+        {
+            System.out.println("Invalid username or password!");
+            System.out.println("Access denied. No operation can be performed.");
+            sc.close();
+            return;
+        }
+
+        System.out.println("Login successful!!");
+        System.out.println("Welcome " + username);
+
+        Banksystem a1 = new Banksystem("1111", 10000.0, "jinkal makwana");
+        Banksystem a2 = new Banksystem("2222", 20000.0, "jaimin makwana");
+
+        System.out.println("Total number of accounts: " + Banksystem.count);
+
+        while (true)
+        {
+            System.out.println("\n1. Check Balance");
             System.out.println("2. Deposit Money");
             System.out.println("3. Withdraw Money");
             System.out.println("4. Exit from system");
             System.out.print("Enter your choice: ");
 
-            choice=sc.nextInt();
+            int choice = sc.nextInt();
 
-            switch(choice){
+            switch (choice)
+            {
                 case 1:
-                    myaccount.checkBalance();
+                    a1.checkBalance();
                     break;
 
                 case 2:
-                    System.out.println("enter deposit amount:");
-                    double depositeamount=sc.nextDouble();
-                    myaccount.depositMoney(depositeamount);
+                    System.out.print("Enter deposit amount: ");
+                    double damount = sc.nextDouble();
+
+                    if (damount > 0)
+                    {
+                        a1.depositMoney(damount);
+                    }
+                    else
+                    {
+                        System.out.println("Invalid deposit amount!");
+                    }
                     break;
 
                 case 3:
-                    System.out.println("enter withdraw amount:");
-                    double withdrawamount=sc.nextDouble();
-                    myaccount.withdrawMoney(withdrawamount);
+                    System.out.print("Enter withdraw amount: ");
+                    double wamount = sc.nextDouble();
+
+                    if (wamount > 0)
+                    {
+                        a1.withdrawMoney(wamount);
+                    }
+                    else
+                    {
+                        System.out.println("Invalid withdraw amount!");
+                    }
                     break;
 
                 case 4:
-                    System.out.println("\nThank you for using our banking system!!");
+                    System.out.println("Thank you for using Bank System!");
+                    sc.close();
                     System.exit(0);
                     break;
 
                 default:
-                    System.out.println("enter choice between 1 to 4");
+                    System.out.println("Invalid choice!");
             }
         }
     }
-    
 }
