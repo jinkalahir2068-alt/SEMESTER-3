4c060619f589917b918fdbcb723c63a34fa0aade
Jinkal Makwana <jinkalahir2068@gmail.com>
2026-09-11 00:15:23 +0530

    Add array_sum.c for calculating sum using pointers
    Implement a program to calculate the sum of n numbers using pointers.

---
+++ Data Structure/Practical-13/array_sum.c
@@ -0,0 +1,18 @@
+//. WAP to calculate the sum of n numbers using Pointer. 
+#include <stdio.h>
+void main(){
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    int *ptr=arr;
+    printf("Enter the elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",ptr+i);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+*(ptr+i);
+    }
+    printf("The sum of the elements is: %d",sum);
+}
