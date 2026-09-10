d5c04fb83cca32fc4771d2b645a1ac2930d71511
Jinkal Makwana <jinkalahir2068@gmail.com>
2026-09-11 00:17:09 +0530

    Add array_sort.c to sort elements using pointers
    Implement array sorting using pointers in C.

---
+++ Data Structure/Practical-13/array_sort.c
@@ -0,0 +1,26 @@
+//WAP to sort the array elements using Pointer. 
+#include <stdio.h>
+void main(){
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    int *ptr=arr;
+    printf("Enter the elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",ptr+i);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(*(ptr+i)>*(ptr+j)){
+                int temp=*(ptr+i);
+                *(ptr+i)=*(ptr+j);
+                *(ptr+j)=temp;
+            }
+        }
+    }
+    printf("The sorted elements are: ");
+    for(int i=0;i<n;i++){
+        printf("%d ",*(ptr+i));
+    }
+}
